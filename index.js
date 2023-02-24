80cda40d58963fea7da575bb6b5b1b289f17334a
Freddy Perez <fred.perez15@gmail.com>
2023-02-24 15:01:33 +0200

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+// const employee = {
+//     name: 'Fred',
+//     streetAddress: 'Stolipin 58'
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+//     return {...employee, [key]: value}
+// }
+
+const employee = {
+    name: "Fred",
+    streetAddress: "40 Thayer",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee,
+        [key]: value,
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee    
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const employee3 = {...employee}
+    delete employee[key]
+    return employee 
+
+}
